b6a8a1ba3318059c9099ca52bcd51aaff681cfa8
Tim Baxter <tim.baxter@cerner.com>
2014-04-04 14:49:46 -0500

    update admin

--- activity_monitor/admin.py
+++ activity_monitor/admin.py
@@ -4,6 +4,6 @@ from activity_monitor.models import Activity
 
 
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'user', 'content_type', 'timestamp')
+    list_display = ('__unicode__', 'actor', 'content_type', 'timestamp')
 
 admin.site.register(Activity, ActivityAdmin)
