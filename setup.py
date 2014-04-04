aac3cf2fe63692d5f5af81506207044741b82966
Tim Baxter <tim.baxter@cerner.com>
2014-04-04 10:35:59 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+#with open('docs/requirements.txt') as f:
+#    required = f.read().splitlines()
+
+setup(
+    name='activity-monitor',
+    version='0.1',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    url='http://github.com/tBaxter/activity-monitor',
+    license='LICENSE.txt',
+    description='A sort of tumblelog-y thing heavily based on code I got somewhere',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    #install_requires=required,
+    zip_safe=False,
+    include_package_data=True,
+    #dependency_links = ['http://github.com/tBaxter/tango-shared-core/tarball/master#egg=tango-shared-0.6']
+)
