a2a411d707aca3d20fa54aec2e4881091835b648
Tim Baxter <tim.baxter@cerner.com>
2014-04-04 14:46:18 -0500

    updated models

--- activity_monitor/models.py
+++ activity_monitor/models.py
@@ -1,21 +1,20 @@
-from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.generic import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-from activity_monitor.managers import ActivityItemManager
-
-UserModel = getattr(settings, "AUTH_USER_MODEL", "auth.User")
+from .managers import ActivityItemManager
 
 
 class Activity(models.Model):
-    user                = models.ForeignKey(UserModel, related_name="timeline_items")
+    actor = models.ForeignKey(get_user_model(), related_name="timeline_items")
+    verb = models.CharField(blank=True, max_length=255)
+    timestamp = models.DateTimeField()
+
+    content_object      = GenericForeignKey()
     content_type        = models.ForeignKey(ContentType)
     object_id           = models.PositiveIntegerField()
-    content_object      = GenericForeignKey()
-    timestamp           = models.DateTimeField()
-    default_string      = models.CharField(blank=True, max_length=255)
-
+    
     objects             = ActivityItemManager()
 
     class Meta:
@@ -28,8 +27,8 @@ class Activity(models.Model):
         return "%s: %s" % (self.content_type.model_class().__name__, unicode(self.content_object))
 
     def get_absolute_url(self):
-        """ Call get_absolute_url method of this object's original content object. """
+        """ 
+        Use original content object's 
+        get_absolute_url method.
+        """
         return self.content_object.get_absolute_url()
-
-    def save(self, force_insert=False, force_update=False):
-        super(Activity, self).save(force_insert=force_insert, force_update=force_update)
