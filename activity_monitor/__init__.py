89fbc1fea63995a61acf2d87eb2b1306e735311d
Tim Baxter <tim.baxter@cerner.com>
2014-11-03 15:41:58 -0600

    Revert "let's get explicit"
    This reverts commit 06c03d49dc3be9e20c6eb2eaf59ae7ee9f8fb844.
    
    Conflicts:
    	activity_monitor/__init__.py

--- activity_monitor/__init__.py
+++ activity_monitor/__init__.py
@@ -3,5 +3,9 @@ try:
     from django.apps import AppConfig
     default_app_config = 'activity_monitor.apps.ActivityMonitorConfig'
 except ImportError:
+<<<<<<< HEAD
     from activity_monitor.models import register_app_activity
+=======
+    from . apps import register_app_activity
+>>>>>>> parent of 06c03d4... let's get explicit
     register_app_activity()
