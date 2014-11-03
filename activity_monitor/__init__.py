5c8b62b8bada6fbb3ed04fd15ec9cb9170ebd1fe
Tim Baxter <tim.baxter@cerner.com>
2014-11-03 15:43:21 -0600

    bad revert

--- activity_monitor/__init__.py
+++ activity_monitor/__init__.py
@@ -3,9 +3,5 @@ try:
     from django.apps import AppConfig
     default_app_config = 'activity_monitor.apps.ActivityMonitorConfig'
 except ImportError:
-<<<<<<< HEAD
-    from activity_monitor.models import register_app_activity
-=======
-    from . apps import register_app_activity
->>>>>>> parent of 06c03d4... let's get explicit
+    from activity_monitor.apps import register_app_activity
     register_app_activity()
