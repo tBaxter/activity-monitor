3c23e1b8b8c869f1370999e88ecd574155b02e3f
Tim Baxter <tim.baxter@cerner.com>
2014-11-03 14:46:09 -0600

    moved registration function

---
+++ activity_monitor/__init__.py
@@ -0,0 +1,7 @@
+# Try/except to catch 1.7 (has AppConfig) or earlier.
+try:
+    from django.apps import AppConfig
+    default_app_config = 'activity_monitor.apps.ActivityMonitorConfig'
+except ImportError:
+    from .apps import register_app_activity
+    register_app_activity()
