3ddb9f2ada2bfedc585d5edb75e7e4bc587203d4
Tim Baxter <tim.baxter@cerner.com>
2014-11-03 15:43:29 -0600

    Revert e2a6445..5c8b62b
    This rolls back to commit e2a644587d99cc2dff8dcaf4f81a06c26683089d.

--- activity_monitor/__init__.py
+++
@@ -1,7 +0,0 @@
-# Try/except to catch 1.7 (has AppConfig) or earlier.
-try:
-    from django.apps import AppConfig
-    default_app_config = 'activity_monitor.apps.ActivityMonitorConfig'
-except ImportError:
-    from activity_monitor.apps import register_app_activity
-    register_app_activity()
