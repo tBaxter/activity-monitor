5a49a9b87742fbab86d7dadae6d99607e509607e
Tim Baxter <tim.baxter@cerner.com>
2015-06-13 13:21:25 -0500

    really clutching at straws here.

--- activity_monitor/models.py
+++ activity_monitor/models.py
@@ -126,3 +126,28 @@ class Activity(models.Model):
             return image.image
         except AttributeError:
             return image
+
+def register_app_activity():
+    """
+    Create watchers for models defined in settings.py.
+    Once created, they will be passed over
+    Activity.objects.follow_model(), which lives in managers.py
+    """
+    from django.conf import settings
+    from django.contrib.contenttypes.models import ContentType
+
+    from .models import Activity
+
+    if not hasattr(settings, 'ACTIVITY_MONITOR_MODELS'):
+        return
+
+    for item in settings.ACTIVITY_MONITOR_MODELS:
+        try:
+            app_label, model = item['model'].split('.', 1)
+            content_type = ContentType.objects.get(app_label=app_label, model=model)
+            model = content_type.model_class()
+            Activity.objects.follow_model(model)
+
+        except ContentType.DoesNotExist:
+            pass
+register_app_activity();
