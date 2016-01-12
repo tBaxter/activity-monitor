446e306005d607763ca1daa8ca0dec42bdad4bd6
TB026891 <tim.baxter@cerner.com>
2016-01-12 12:40:17 -0600

    updates for 1.8/1.9

--- setup.py
+++ setup.py
@@ -7,17 +7,14 @@ from setuptools import find_packages
 
 setup(
     name='activity-monitor',
-    version='0.11.3',
+    version='0.12.0',
     author=u'Tim Baxter',
     author_email='mail.baxter@gmail.com',
     url='http://github.com/tBaxter/activity-monitor',
     license='MIT',
-    description='A sort of tumblelog-y thing heavily based on code I got somewhere',
+    description='A sort of tumblelog-y thing heavily based on code I got somewhere.',
     long_description=open('README.md').read(),
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    dependency_links = [
-       'http://github.com/tBaxter/django-voting/tarball/master#egg=tango-voting-0.1',
-    ]
 )
