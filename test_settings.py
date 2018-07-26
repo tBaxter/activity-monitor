b944191cc926a3a932f194209bfa299b61c0f93b
TBaxter <mail.baxter@gmail.com>
2018-07-26 16:51:13 -0500

    Trying to find templates

--- test_settings.py
+++ test_settings.py
@@ -1,3 +1,21 @@
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            '../templates',
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 SECRET_KEY = "lorem ipsum"
 
 INSTALLED_APPS = (
